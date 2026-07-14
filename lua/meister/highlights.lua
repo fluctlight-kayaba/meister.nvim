b080275d347d04a0f085762af690eb775595f919
Son Le <fluctlight-kayaba@users.noreply.github.com>
2026-07-14 17:24:12 +0700

    feat(highlights): card and ghost-number groups

--- lua/meister/highlights.lua
+++ lua/meister/highlights.lua
@@ -2,12 +2,18 @@ local M = {}
 
 local groups = {
 	MeisterInputAccent = { fg = "#e0af68" },
+	MeisterCardBorder = { link = "Comment" },
+	MeisterCardText = { link = "Comment" },
 }
 
 function M.setup()
 	local function apply()
 		for name, val in pairs(groups) do
 			vim.api.nvim_set_hl(0, name, vim.tbl_extend("keep", { default = true }, val))
+		end
+		local normal = vim.api.nvim_get_hl(0, { name = "Normal" })
+		if normal.bg then
+			vim.api.nvim_set_hl(0, "MeisterGhostNr", { fg = normal.bg })
 		end
 	end
 	apply()
